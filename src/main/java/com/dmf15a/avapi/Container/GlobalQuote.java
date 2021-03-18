ef7f87e50d11368425f9f0afb6b922733342c73b
DavidM-Fox <david.mac.fox@gmail.com>
2021-03-17 20:55:43 -0500

    Remove ApiQuery parent
    - Make GlobalQuote only a container (Doesn't do API calls)
    - Refactor constructors
    - Cleanup

--- src/main/java/com/dmf15a/avapi/Container/GlobalQuote.java
+++ src/main/java/com/dmf15a/avapi/Container/GlobalQuote.java
@@ -2,103 +2,40 @@ package com.dmf15a.avapi.Container;
 
 import com.dmf15a.avapi.ApiQuery;
 import com.dmf15a.avapi.Printer.TablePrinter;
-import org.apache.commons.io.IOUtils;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
-import static com.dmf15a.avapi.Utils.toUnixTimestamp;
-
-public class GlobalQuote extends ApiQuery {
+public class GlobalQuote {
 
     public class MetaInfo {
         public String symbol;
         public String market;
         public String title;
         public ArrayList<String> headers;
-        public MetaInfo() {
-            this.symbol = "";
-            this.market = "";
-            this.title = "";
+        public MetaInfo(String... args) {
+            this.symbol = args[0];
+            this.market = args[1];
+            this.title = args[2];
             this.headers = new ArrayList<>();
         }
     }
 
-    public MetaInfo info;
+    public GlobalQuote.MetaInfo info;
     public long timestamp;
     public ArrayList<Float> data;
 
     public GlobalQuote() {
-        super("");
-        this.info = new MetaInfo();
+        this.info = new MetaInfo("", "", "");
         this.timestamp = 0;
         this.data = new ArrayList<>();
     }
 
-    public GlobalQuote(String symbol, String apiKey) throws IOException {
-        super(apiKey);
-        this.info = new MetaInfo();
-        this.info.symbol = symbol;
-        this.info.market = "USD";
-        this.info.title = new StringBuilder().append(symbol).append(": GLOBAL_QUOTE (USD)").toString();
+    public GlobalQuote(String symbol) {
+        this.info = new MetaInfo(symbol, "USD", symbol +
+                ": GLOBAL_QUOTE (USD)"
+        );
         this.data = new ArrayList<>();
-        update();
-    }
-
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    public void update() throws IOException {
-
-        if (getApiKey() == "") {
-            System.err.println(new StringBuilder("WARNING: No API Key set\n")
-                    .append("\tat com.dmf15a.avapi.Company.Stock.GlobalQuote")
-                    .toString());
-            return;
-        }
-
-        // Only three parameters needed for GlobalQuote
-        addQuery("function", "GLOBAL_QUOTE");
-        addQuery("symbol", info.symbol);
-        addQuery("datatype", "csv");
-
-        // Query Alpha Vantage API and get response
-        StringWriter csv = new StringWriter();
-        IOUtils.copy((InputStream) getResponse(), csv, "UTF-8");
-
-        // Parse csv data
-        try (BufferedReader reader = new BufferedReader(new StringReader(csv.toString()))) {
-
-            // Get headers
-            String line = reader.readLine();
-            info.headers = new ArrayList<>(Arrays.asList(line.split(",")));
-
-            // Get data
-            line = reader.readLine();
-            ArrayList<String> data = new ArrayList<>(Arrays.asList(line.split(",")));
-
-            // Swap symbol/timestamp columns
-            Collections.swap(info.headers, 0, 6);
-            Collections.swap(data, 0, 6);
-
-            // Remove symbol column, convert data into Floats
-            info.headers.remove(6);
-            data.remove(6);
-
-            // Convert/remove timestamp data column
-            timestamp = toUnixTimestamp(data.get(0));
-            data.remove(0);
-
-            String percent = data.get(7).replaceAll("%", "");
-            data.set(7, percent);
-
-            for (String value : data) {
-                this.data.add(Float.parseFloat(value));
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public void print() {
